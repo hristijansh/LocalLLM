6c342804f832a51e5f509a5674c077c51e3c8e2b
hristijansh <hslakeski@gmail.com>
2025-07-01 12:42:47 +0200

    Update task-1.py

--- task-1.py
+++ task-1.py
@@ -1,1 +1,19 @@
-# Solution for task-1
+def my_function():
+    even_numbers = []
+    for i in range(1, 11):
+        if i % 2 == 0:
+            even_numbers.append(i)
+    print()        
+    print("Even numbers up to 10 are:")
+    print(even_numbers)
+
+
+    sum_even = 0
+    for number in even_numbers:
+        sum_even += number
+        
+    print()
+    print("Sum of even numbers up to 10 is:")
+    print(sum_even)
+
+my_function()
